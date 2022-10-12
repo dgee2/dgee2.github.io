3a77093bea75f409001228b6168b86f45bd41a9f
Daniel Gee <dan@dan-gee.co.uk>
2022-10-12 10:02:57 +0100

    Add google analytics

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -1,15 +1,35 @@
-const { description } = require("../../package");
+const { name, description } = require("../../package");
 
 module.exports = {
-  title: "dan-gee.co.uk",
+  title: name,
   description: description,
   theme: "@vuepress/theme-blog",
-  head:[
-    ['script',{type:'text/javascript'},`(function(c,l,a,r,i,t,y){
+  head: [
+    [
+      "script",
+      { type: "text/javascript" },
+      `(function(c,l,a,r,i,t,y){
       c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
       t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
       y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-  })(window, document, "clarity", "script", "e1g2hgog5e");`]
+  })(window, document, "clarity", "script", "e1g2hgog5e");`,
+    ],
+    [
+      "script",
+      {
+        async: true,
+        src: "https://www.googletagmanager.com/gtag/js?id=G-V1DTD02KYD",
+      },
+    ],
+    [
+      "script",
+      {},
+      `window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-V1DTD02KYD');`,
+    ],
   ],
   themeConfig: {
     sitemap: {
@@ -51,5 +71,11 @@ module.exports = {
    * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
    */
   plugins: [
+    [
+      "@vuepress/google-analytics",
+      {
+        ga: "G-V1DTD02KYD",
+      },
+    ],
   ],
 };
